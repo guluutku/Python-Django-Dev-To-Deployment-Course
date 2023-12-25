614d3ca53419749e6a956bc71069bd2ce5ab6108
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-12-25 15:22:36 +0300

    Putting variables in print

--- strings.py
+++ strings.py
@@ -8,5 +8,16 @@ print('Hello I am ' + name + ' age ' + str(age1))
 
 print("-----------------")
 
-# Argument by positions
+# Arguments by positions
 print('{}, {}, {}'.format('a', 'b', 'c'))
+print('{1}, {2}, {0}'.format('a', 'b', 'c'))
+
+print("-----------------")
+
+# Arguments by name
+print('My name is {name} and I am {age2} years old.'.format(name=name, age2=age2))
+
+print("-----------------")
+
+# F-Strings (only 3.6+)
+print(f'My name is {name} and I am {age2} years old.')
