40bb95898bdfbead0b57b846f3b86b5e73e4ad1c
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-12-25 15:23:11 +0300

    String methods

--- strings.py
+++ strings.py
@@ -5,19 +5,23 @@ age2 = 26
 
 # Concatenate
 print('Hello I am ' + name + ' age ' + str(age1))
-
 print("-----------------")
 
 # Arguments by positions
 print('{}, {}, {}'.format('a', 'b', 'c'))
 print('{1}, {2}, {0}'.format('a', 'b', 'c'))
-
 print("-----------------")
 
 # Arguments by name
 print('My name is {name} and I am {age2} years old.'.format(name=name, age2=age2))
-
 print("-----------------")
 
 # F-Strings (only 3.6+)
 print(f'My name is {name} and I am {age2} years old.')
+print("-----------------")
+
+# String Methods
+s = 'Hello there world'
+
+# Capitalize
+
