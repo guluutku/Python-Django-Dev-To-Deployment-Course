c5cea443246e39a1009792ceba3996463b994c7e
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-12-25 21:07:56 +0300

    Function examples

---
+++ functions.py
@@ -0,0 +1,28 @@
+
+# Different from other langs. Python uses identation with tabs and spaces. No brackets, curly brackets
+
+# Create Function
+def sayHello():
+    print('Hello')
+
+sayHello()
+print('----------------------')
+
+def sayHello2(name = 'Sam'):
+    print('Hello ' + name)
+
+sayHello2()
+sayHello2('Beth')
+print('----------------------')
+
+# Return
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+print(getSum(1, 2))
+print('----------------------')
+print()
+print()
+
+
