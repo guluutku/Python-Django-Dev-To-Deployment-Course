2192ababe0c05818506b23a2a624c2c9f2f378d1
Gün ULUUTKU <gunuluutku@gmail.com>
2023-12-25 23:24:32 +0300

    Update conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+
+x = 10
+y = 6
+
+# Comparison Operators
+
+# if-else Conditions
+if x == y:
+    print(f'{x} equal to {y}')
+elif x < y:
+    print(f'{x} less to {y}')
+elif x > y:
+    print(f'{x} greater to {y}')
+else:
+    print('not equals')
+    
