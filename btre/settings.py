fe2bbf929d1976fbb4eaf7c411153f318522d86a
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-12-27 19:31:45 +0300

    Added listings page

--- btre/settings.py
+++ btre/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
-    'search.apps.SearchConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
