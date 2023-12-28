c6ed55b4283e7cec750ca1d5ae39429d906ad8bb
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-12-28 16:30:00 +0300

    Login registered user

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 def register(request):
@@ -51,8 +51,18 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        print('SUBMITED RLOGG')
-        return redirect('login')
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'Logged In')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid User')  
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
