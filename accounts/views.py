e12637dbb737aebe18ae64d624b9552f340bd1cd
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-12-28 16:54:19 +0300

    Change Navigator Bar when logged in and logged out

--- accounts/views.py
+++ accounts/views.py
@@ -67,7 +67,11 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'Logged Out')
+        return redirect('index')
 
 def dashboard(request):
+    
     return render(request, 'accounts/dashboard.html')
